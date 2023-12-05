9c4709b8463f5fe34825ed84cb32496326f19bb2
gustavthron <gustavthron@outlook.com>
2023-12-05 14:29:53 +0100

    Refactor day 1 solution

--- day01/aoc.ps1
+++ day01/aoc.ps1
@@ -1,31 +1,91 @@
-$inputContent = Get-Content ./input.txt
-$part = $ENV:part
-
-Write-Host "PowerShell"
-if ($part -eq "part1") {
-  $result = 0
-  $inputContent | foreach {
-    $_ -match '^\D*(\d).*?(\d)?\D*$' > $null
-    $result += "$($Matches.1)$($Matches.2 -ne $null ? $Matches.2 : $Matches.1)"
-  }
-  Write-Host $result
-} elseif ($part -eq "part2") {
-  $digitMapping = @{
-    'zero'  = 0
-    'one'   = 1
-    'two'   = 2
-    'three' = 3
-    'four'  = 4
-    'five'  = 5
-    'six'   = 6
-    'seven' = 7
-    'eight' = 8
-    'nine'  = 9
-  }
-  $result = 0
-  $inputContent | foreach {
-    $m = $_ | Select-String -Pattern "(?=(\d|zero|one|two|three|four|five|six|seven|eight|nine))" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value } | ForEach-Object { $digitMapping.ContainsKey($_) ? $digitMapping[$_] : $_ }
-    $result += "$($m[0])$($m[-1])"
-  }
-  Write-Host $result
+$SpelledDigitValues = @{
+  'zero'  = 0
+  'one'   = 1
+  'two'   = 2
+  'three' = 3
+  'four'  = 4
+  'five'  = 5
+  'six'   = 6
+  'seven' = 7
+  'eight' = 8
+  'nine'  = 9
 }
+
+function ConvertTo-Digit {
+  param (
+    [string]
+    $PossiblySpelledDigit
+  )
+  return $SpelledDigitValues.Contains($PossiblySpelledDigit) ? $SpelledDigitValues[$PossiblySpelledDigit] : [int]$PossiblySpelledDigit
+}
+
+function Get-Digits {
+  process {
+    $_ |
+      Select-String -Pattern '\d' -AllMatches |
+      ForEach-Object { $_.Matches } |
+      ForEach-Object { $_.Value }
+  }
+}
+
+function Get-SpelledDigitsAndDigits {
+  process {
+    $_ |
+      Select-String -Pattern "(?=(\d|$($SpelledDigitValues.Keys -join '|')))" -AllMatches |
+      ForEach-Object { $_.Matches } |
+      ForEach-Object { $_.Groups[1].Value }
+  }
+}
+
+function New-CalibrationValue {
+  param (
+    [int[]]
+    $LineDigits
+  )
+  return $LineDigits[0] * 10 + $LineDigits[-1]
+}
+
+function Invoke-Part1Solution {
+  param (
+    [string[]]
+    $InputContent
+  )
+  $CalibrationValueSum = 0
+  $InputContent | ForEach-Object {
+    $LineDigits = $_ | Get-Digits
+    $CalibrationValueSum += New-CalibrationValue -LineDigits $LineDigits
+  }
+  Write-Host $CalibrationValueSum
+}
+
+function Invoke-Part2Solution {
+  param (
+    [string[]]
+    $InputContent
+  )
+  $CalibrationValueSum = 0
+  $InputContent | ForEach-Object {
+    $LineDigits = $_ |
+      Get-SpelledDigitsAndDigits |
+      ForEach-Object { ConvertTo-Digit -PossiblySpelledDigit $_ }
+    $CalibrationValueSum += New-CalibrationValue -LineDigits $LineDigits
+  }
+  Write-Host $CalibrationValueSum
+}
+
+function Invoke-AdventOfCode {
+  Write-Host 'PowerShell'
+  $InputContent = Get-Content ./input.txt
+  $Part = $ENV:part
+
+  switch ($Part) {
+    'part1' {
+      Invoke-Part1Solution -InputContent $InputContent
+    }
+    'part2' {
+      Invoke-Part2Solution -InputContent $InputContent
+    }
+  }
+}
+
+Invoke-AdventOfCode
